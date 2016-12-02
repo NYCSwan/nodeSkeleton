aeedb1d06e77fba539f365c454289ba3a42eeaba
NYCSwan <megan.swanby@gmail.com>
2016-12-02 10:28:46 -0500

    Update with get and post routes on router file.with errors, json and html formating

--- routes/routesController.js
+++ routes/routesController.js
@@ -14,3 +14,54 @@ router.use(methodOverride(function(req, res){
         return method
       }
 }))
+
+router.route('/')
+// GET all models
+	.get(function(req, res, next){
+		// retrieve all items from the db
+		mongoose.model(Model).find({}, function(err, models) {
+			if (err) {
+				return conosle.error(err);
+			} else {
+				// respond to html and json 
+				html: function(){
+					res.render('models/index', {
+						title: 'All the Models',
+						"models": models
+					});
+				},
+				json: function(){
+					res.json(models);
+				}
+			});	
+		}
+	});
+	// post new thing
+	.post(function(req, res){
+		// get values from post req through forms or rest calls.
+		var name = req.body.name;
+		// call the create function for db
+		mongoose.model('Model').create({
+			name: name
+		}, function(err, model){
+			if (err) {
+				res.send("there was a problem adding the info to the db");
+			} else {
+				// model instance has been created
+				  console.log('POST creating new instance of model: ' + model);
+				res.format({
+				//HTML response will set the location and redirect back to the home page or wherever you want (success page)
+					html: function(){
+						// If it worked, set the header so the address bar doesn't still say /adduser
+						res.location("models");
+						// And forward to success page
+						res.redirect("/models");
+					},
+				//JSON response will show the newly created blob
+					json: function(){
+						res.json(model);
+					}
+				});
+			}
+		})
+	});
